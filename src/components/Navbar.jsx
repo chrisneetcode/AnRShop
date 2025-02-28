b6132cc763781dd977eb5c30826a1306453db293
chrisneetcode <rchrisorallo@gmail.com>
2025-02-28 21:56:09 +0800

    Some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,22 @@ const Navbar = () => {
   return (
     <div>
       {/* Upper Navbar */}
-<div className="bg-orange-100 py-1">
-  <div className="container flex justify-between items-center">
-    <div className="perfume-shop-title text-2xl sm:text-3xl md:text-4xl mx-4 my-2">
-      Perfume Shop
-    </div>
+      <div className="bg-orange-100 py-1">
+        <div className="container flex justify-between items-center">
+          {/* Group logo and navbar items together */}
+          <div className="flex items-center space-x-4">
+            <div className="perfume-shop-title md:text-4xl mx-6 my-1">
+              Perfume Shop
+            </div>
 
-    <ul className="flex space-x-4 ">
-      <li>Home</li>
-      <li>About</li>
-      <li>Services</li>
-      <li>Contact</li>
-    </ul>
-  </div>
-</div>
-
-
-      {/* Lower Navbar */}
-      <div>
+            <ul className="flex space-x-4">
+              <li>Home</li>
+              <li>About</li>
+              <li>Services</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </div>
         <div className="group">
           <input
             type="text"
@@ -31,9 +29,14 @@ const Navbar = () => {
           duration-60 rounded-full border
           border-gray-300 px-1 py-2
           focus:outline-none focus:border-3
-          focus:border-orange-100"
+          focus:border-orange-300"
           />
         </div>
+      </div>
+
+      {/* Lower Navbar */}
+      <div>
+
       </div>
 
     </div>
